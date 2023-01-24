9a5d9fa838832a992f0acc46095693e6dbe6930d
Joshua Hahn <114379383+FooFooTheSnoo@users.noreply.github.com>
2023-01-24 11:28:45 -0800

    added volunteer function

--- javascript/app.js
+++ javascript/app.js
@@ -1,32 +1,42 @@
 
 function name () {
-  let name = prompt("What is your name?");
+
+  let name = prompt("Hello! What is your name?");
   alert("Welcome Back! " + name);
  
 }
 
-// Function to be applied later
-function amountOfCats() {
-    let amountOfCats = prompt("How many cats would you like to adopt?");
-    alert("Wow! Thank you for adopting " + amountOfCats + " cats!");
+function volunteer() {
+  let volunteer = prompt("Would you like volunteer at your local pet shelter? yes/no");
+
+  if ( volunteer==="yes" ) {
+    alert("Please contact your local pet shelter to let them know!");
+  }
+
+  else if ( volunteer==="no" ) {
+    alert("Please consider volunteering at a later time! We would love your company!");
+  }
 }
 
+
 function amountOfDogs() {
-  let amountOfDogs = prompt("How many dogs would you like to adopt?");
+
+let amountOfDogs = prompt("How many dogs would you like to adopt?");
   alert("Wow! Thanks for adopting " + amountOfDogs + " dogs!");
 }
 
 function dogSex() {
   let dogSex = prompt("Would you like a male or female dog?");
+  
   if (dogSex==="male") {
     alert("Great! 20 male dogs waiting to be adopted");
 }
 
-else if ( dogSex === "female" ) {
-    alert("Great! We have 30 females waiting to be adopted")
+  else if ( dogSex === "female" ) {
+    alert("Great! We have 30 females waiting to be adopted");
 }
 
-else {
-    alert("Oops! Please try again!")
+  else {
+    alert("Oops! Please try again!");
 }
 }
