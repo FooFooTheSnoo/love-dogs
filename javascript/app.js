7b2afbcd07c786bb52654d03e42c9d7abaa36741
Joshua Hahn <114379383+FooFooTheSnoo@users.noreply.github.com>
2023-01-24 09:18:54 -0800

    edited text/layout/links

--- javascript/app.js
+++ javascript/app.js
@@ -1,19 +1,32 @@
-let name = prompt("What is your name?");
-alert("Welcome Back! " + name);
 
-let AmountOfDogs = prompt("How many dogs would you like to adopt?");
-alert("Wow! Thanks for adopting " + AmountOfDogs + " dogs!");
+function name () {
+  let name = prompt("What is your name?");
+  alert("Welcome Back! " + name);
+ 
+}
 
-let DogSex = prompt("Would you like a male or female dog?");
-if (DogSex==="male") {
+// Function to be applied later
+function amountOfCats() {
+    let amountOfCats = prompt("How many cats would you like to adopt?");
+    alert("Wow! Thank you for adopting " + amountOfCats + " cats!");
+}
+
+function amountOfDogs() {
+  let amountOfDogs = prompt("How many dogs would you like to adopt?");
+  alert("Wow! Thanks for adopting " + amountOfDogs + " dogs!");
+}
+
+function dogSex() {
+  let dogSex = prompt("Would you like a male or female dog?");
+  if (dogSex==="male") {
     alert("Great! 20 male dogs waiting to be adopted");
 }
 
-else if (DogSex==="female") {
+else if ( dogSex === "female" ) {
     alert("Great! We have 30 females waiting to be adopted")
 }
 
 else {
     alert("Oops! Please try again!")
 }
-
+}
